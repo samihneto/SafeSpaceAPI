e55ce5ac145402b682d2336ecb62536c8ca63a52
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2025-06-01 16:46:35 -0300

    Alterando criação das migrations

--- Program.cs
+++ Program.cs
@@ -38,6 +38,13 @@ public class Program
 
         var app = builder.Build();
 
+        // Aplicar migrations automaticamente
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<SafeSpaceContext>();
+            context.Database.Migrate();
+        }
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
